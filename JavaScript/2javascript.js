11e5292080f949fdaad6e3f817d2f3345c080387
aniketsurv <Aniket#0903>
2025-02-28 12:33:56 +0530

    first commit

---
+++ JavaScript/2javascript.js
@@ -0,0 +1,2 @@
+document.write(`I am external javascript file used for the externally`);
+//this is js fileqqqqqqqqqqqqq
---
+++ JavaScript/5var.js
@@ -0,0 +1,7 @@
+var a=10;
+var b=20;
+document.write(a+b);
+document.write(a-b);
+
+
+document.getElementById("annik").innerHTML="This is aniket";
